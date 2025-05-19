021777e6241975076aac51f2f6a7ca6d406245e1
ucasvieira <lucasvieirasouza5@gmail.com>
2025-05-19 14:02:29 -0300

    Estilização CSS

--- ExpressJsProject/app.js
+++ ExpressJsProject/app.js
@@ -1,26 +1,21 @@
-const http = require('http');
-
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/',(req,res,next) => {
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
-app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product',(req,res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next) => {
-    res.send('<h1>Hello World</h1>');
-});
-
 app.listen(3000);
+
+module.exports = path.dirname(require.main.filename);

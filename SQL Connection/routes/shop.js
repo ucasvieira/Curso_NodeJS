fa95b37fa5197e341f1febda597580b317a3c52c
ucasvieira <lucasvieirasouza5@gmail.com>
2025-05-28 11:41:02 -0300

    SQL Basic connection

---
+++ SQL Connection/routes/shop.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const path = require('path');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct); // Assuming you have a method for product details
+router.get('/cart',shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.get('/orders',shopController.getOrders);
+router.get('/checkout',shopController.getCheckout);
+
+module.exports = router;

4d476f0b65e757a25884b66eda6b81aded953380
ucasvieira <lucasvieirasouza5@gmail.com>
2025-06-10 11:02:27 -0300

    orders added

---
+++ Sequelize/util/database.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete' , 'root', 'acerti12@', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
